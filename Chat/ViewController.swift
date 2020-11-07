05e7c957af4db4cfb4c7f5249e260d8c8f0880d1
Sabina <rigby917@gmail.com>
2020-11-07 16:49:22 +0300

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Robins Frieda on 11/7/20.
+//  Copyright © 2020 Robins Frieda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
